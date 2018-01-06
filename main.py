179b5d321057edd58c7745c5ebb956b8a44ea555
Joe Thorpe <thorpj@users.noreply.github.com>
2018-01-06 22:47:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ client = discord.Client()
 #cfg_path = os.path.join(".", "config.json")
 
 
-NAME = "change_nickname"
+NAME = "rename_user"
 PREFIX = "!r"
 commands = {}
 
-my_user_id = "138565526799515648"
-server_id = "376745782566453249"
+server_id = os.environ["discord_rename_user_server_id"]
 
 def _get_server_obj(server_id):
     logger.debug(server_id)
@@ -30,10 +29,6 @@ def load_config(path):
 def save_config(path, config):
     with open(path, 'w') as f:
         f.write(json.dump(config))
-
-def get_name(user_id):
-    user = _get_user_obj(user_id)
-    return user.name
 
 def find_member(nickname, server):
     member = discord.utils.get(server.members, display_name=nickname)
