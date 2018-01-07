5be322e44104c11a1ee0560af1e0442668ac1c1a
thorpj <thorpejoe4@gmail.com>
2018-01-07 11:17:59 +0800

    Fixed issue with delimiter

--- main.py
+++ main.py
@@ -108,6 +108,9 @@ async def on_message(message):
         new_item = []
         for item in items:
             if item == delimiter:
+                if item = "\delimiter":
+                    new_item.append(item[1:])
+                    continue
                 new_items.append(' '.join(new_item))
                 logger.debug(' '.join(new_item))
                 new_item = []
